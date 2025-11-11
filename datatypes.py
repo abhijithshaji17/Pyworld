6488bcb89a90f299646d9e6653a4146d0c508bfa
abhijithshaji17 <abhijithshaji700@gmail.com>
2025-11-11 22:56:19 +0530

    new

---
+++ datatypes.py
@@ -0,0 +1,17 @@
+#Data-types
+a = -9
+b = 4 + 3j
+c = True
+d = "hi"
+e = [1,2,3,4]
+f = (1,2,3,4)
+g = {9,2}
+h = 9.12
+print (type(a))
+print (type(b))
+print (type(c))
+print (type(d))
+print (type(e))
+print (type(g))
+print (type(h))
+
