9bc61b23f972a3c179a7f48cc813ca69731ea91a
abhijithshaji17 <abhijithshaji700@gmail.com>
2025-11-11 18:23:09 +0530

    new file

---
+++ reversenum.py
@@ -0,0 +1,6 @@
+#Reverse number
+n = int(input("Enter a number: "))
+rev_num = int(str(n)[::-1])
+print ("Entered number is", n)
+print ("its reverse is", rev_num)
+
---
+++ strreplace.py
@@ -0,0 +1,5 @@
+#Replace a string
+fact = "Hello, this world is really kind."
+reality = fact.replace("kind","cruel, sometimes")
+print (reality)
+
