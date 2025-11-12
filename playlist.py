71041ce1522e9cc8834f4c3d2fbb1a474cb38215
abhijithshaji17 <abhijithshaji700@gmail.com>
2025-11-12 22:08:02 +0530

    new

---
+++ playlist.py
@@ -0,0 +1,10 @@
+#Basic List traversing, slicing, reversing
+li = [23, 45, 61, 98, 34]
+li.append(56)
+print (li)
+print (li.index(56))
+print (li[::-1])
+print (li[0:2])
+print (35 in li)
+print (li[3])
+
