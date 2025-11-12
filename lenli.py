40dbb4c147ae4350746c3bbb5760c06ad1dca0f2
abhijithshaji17 <abhijithshaji700@gmail.com>
2025-11-12 22:38:24 +0530

    new

---
+++ lenli.py
@@ -0,0 +1,15 @@
+#List length, etc.
+proverb = ["An Apple", "a day", "keeps the doctor away!"]
+se = [len(word) for word in proverb]
+print (se)
+
+numbers = [1, 2, 3, 4, 5, 6]
+squares = [a**2 for a in numbers]
+print (squares)
+
+
+
+
+
+
+
