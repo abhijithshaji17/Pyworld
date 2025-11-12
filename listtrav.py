4a96dabadd11712c5b9d6250e0b67d499720b9db
abhijithshaji17 <abhijithshaji700@gmail.com>
2025-11-12 23:34:39 +0530

    new

---
+++ listtrav.py
@@ -0,0 +1,13 @@
+#List Traversal
+fruit = ['apple', 'mango', 'orange', 'banana' ]
+i = 0
+while i < len(fruit):
+      print (fruit[i])
+      i = i + 1
+
+print ("carrot" in fruit)
+
+fruit = "apple"
+for char in fruit:
+    print (char)
+
