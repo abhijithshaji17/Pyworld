84d025c71f471501eddf16dc57646beac580d152
abhijithshaji17 <abhijithshaji700@gmail.com>
2025-11-13 08:10:41 +0530

    newwala

---
+++ ktu17.3.py
@@ -0,0 +1,11 @@
+#Predict output
+for count in range(10, 0, -2):
+   print (count, end = " ")
+
+def greet_user():
+	username = input("Enter name: ")
+	greet = ("Hello", username)
+	return greet
+print (greet_user())
+print (greet[::-1])
+
