398902698b2af6f7d6e49823686fe12ccf9bf538
abhijithshaji17 <abhijithshaji700@gmail.com>
2025-11-13 22:36:38 +0530

    examprep

---
+++ asgnqn8.py
@@ -0,0 +1,7 @@
+#Predicting Output
+
+#Q8.
+s = "banana"
+print (s.replace("a", "o"))
+print (s.count("o"))
+
---
+++ asgnqn9.py
@@ -0,0 +1,12 @@
+#Predicting Output
+s1 = "try"
+s2 = "again"
+n1 = 8
+n2 = 5
+print (s1+s2)
+print (s1*3)
+print (s1,s2)
+print (len(s2)+n2)
+print (n1+n2)
+
+
