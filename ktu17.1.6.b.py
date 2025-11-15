9542e5876148621d3bad83d88160c9f927a87954
abhijithshaji17 <abhijithshaji700@gmail.com>
2025-11-15 23:26:14 +0530

    examprep

---
+++ ktu17.1.6.b.py
@@ -0,0 +1,8 @@
+'''
+You are given a string, for instance, 'How are you', Split the string on to a (space) delimiter and join using a '-' (hyphen) delimiter. (final output of the given example will be 'How-are-you').
+'''
+dia1 = "Kaise mai kahun dekh na sakun anjaane raastee..."
+dia2 = dia1.split( )
+dia11 = "-".join(dia2)
+print(dia11)
+
