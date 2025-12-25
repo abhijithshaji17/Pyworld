2f33018e7fa08af9611e5d9f4e15f67287aab400
abhijithshaji17 <abhijithshaji700@gmail.com>
2025-12-25 22:43:49 +0530

    learning

---
+++ p4.py
@@ -0,0 +1,18 @@
+'''
+Write a python program to display the shipping fee. Shipping fee is calculate based on the following criteria:
+Read the weight of the parcel from the user
+Fee is $5 if weight <=1.
+Fee is $10 if weight <= 5.
+Fee is $20 otherwise.
+'''
+
+def shipping_fee():
+	if weight <=1:
+	  return "$5"
+	elif weight <= 5:
+          return "$10"
+	else:
+	  return "$20"
+weight = float(input("Enter your weight: "))
+print ("shipping fee is: ", shipping_fee())
+
