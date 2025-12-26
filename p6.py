83943def04326ec501e883800835322b24f3b157
abhijithshaji17 <abhijithshaji700@gmail.com>
2025-12-26 12:57:15 +0530

    learning

--- p6.py
+++ p6.py
@@ -1,10 +1,10 @@
 #palindrome
 
-def palindrome_check():
-
+def palindrome_check(a):
 	if a == a[::-1]:
-  	  print (a, "is palindrome")
+		print(a, "is palindrome")
 	else:
-  	  print (a, "is not palindrome")
+		print(a, "is not palindrome")
+
 a = input("Enter something: ")
-palindrome_check()	
+palindrome_check(a)
