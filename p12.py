8d07631135428cc6ce6dbdf6040caecf90271133
abhijithshaji17 <abhijithshaji700@gmail.com>
2025-12-28 19:28:53 +0530

    learning

---
+++ p12.py
@@ -0,0 +1,8 @@
+'''
+Given three inputs that are stored in the variables a, b, and c. You need to print a a times and b b times  in a single line separated by c.
+'''
+a = "8"
+c = "&"
+b = "1"
+print (a*3 + c*3 + b*3)
+
